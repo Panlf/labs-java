6beaee0044b147ab6a37bcea956b73b5354d85cc
panlf <liangfeng_pan@163.com>
2026-05-19 23:35:58 +0800

    add: Java的NIO学习测试代码

---
+++ src/main/java/com/labs/nio/TestNIOBlocking_2.java
@@ -0,0 +1,73 @@
+package com.labs.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * NIO Blocking 测试2
+ * @author panlf
+ * @date 2026/5/19
+ */
+public class TestNIOBlocking_2 {
+	//客户端
+	public void client() throws IOException{
+		SocketChannel sChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 8080));
+		
+		FileChannel inChannel = FileChannel.open(Paths.get("E://temp//38.jpg"), StandardOpenOption.READ);
+		
+		ByteBuffer buf = ByteBuffer.allocate(1024);
+		
+		while(inChannel.read(buf) != -1){
+			buf.flip();
+			sChannel.write(buf);
+			buf.clear();
+		}
+		
+		sChannel.shutdownOutput();
+		
+		//接收服务端的反馈
+		int len = 0;
+		while((len = sChannel.read(buf)) != -1){
+			buf.flip();
+			System.out.println(new String(buf.array(), 0, len));
+			buf.clear();
+		}
+		
+		inChannel.close();
+		sChannel.close();
+	}
+	
+	//服务端
+	public void server() throws IOException{
+		ServerSocketChannel ssChannel = ServerSocketChannel.open();
+		
+		FileChannel outChannel = FileChannel.open(Paths.get("E://temp//COPY--38.jpg"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+		
+		ssChannel.bind(new InetSocketAddress("127.0.0.1", 8080));
+		
+		SocketChannel sChannel = ssChannel.accept();
+		
+		ByteBuffer buf = ByteBuffer.allocate(1024);
+		
+		while(sChannel.read(buf) != -1){
+			buf.flip();
+			outChannel.write(buf);
+			buf.clear();
+		}
+		
+		//发送反馈给客户端
+		buf.put("服务端接收数据成功".getBytes());
+		buf.flip();
+		sChannel.write(buf);
+		
+		sChannel.close();
+		outChannel.close();
+		ssChannel.close();
+	}
+}

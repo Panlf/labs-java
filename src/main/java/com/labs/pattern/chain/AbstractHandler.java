bcf7f73104327ae8164dea17668c48254b557ab3
panlf <liangfeng_pan@163.com>
2026-06-10 21:43:13 +0800

    add: 增加java设计模式-责任链模式

---
+++ src/main/java/com/labs/pattern/chain/AbstractHandler.java
@@ -0,0 +1,30 @@
+package com.labs.pattern.chain;
+
+import lombok.Data;
+
+/**
+ * 责任链抽象类
+ * @author panlf
+ * @date 2022/7/11
+ */
+@Data
+public abstract class AbstractHandler<T> {
+    protected String name;
+
+    protected AbstractHandler<T>  nextHandler;
+
+    public AbstractHandler(String name){
+        this.name = name;
+    }
+
+    public boolean hasNextHandler(){
+        return nextHandler != null;
+    }
+
+    /**
+     * 业务处理方法
+     * @param t 传入参数
+     * @return
+     */
+    public abstract T process(T t);
+}
